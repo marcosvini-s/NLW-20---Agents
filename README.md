f680bfede0c3c4c7cffa73a282c6eae26808f27c
Marcos Vinicius <marcos.sampaiopeixoto@hotmail.com>
2025-07-31 12:13:08 -0300

    add AI response

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-#
+# API
+- Aplication
+- Programming
+- Interface
+- JSON: Java Object Notation 
+
+# HTTP
+- HyperText
+- Transfer
+    - Transferencia 
+- Protocol
+    - Protocolo
+    - Conjunto de regras
+- Methods HTTP (verbos): Get, Post, Patch/Put, Delete
+- Heders (cabeçalhos): Instruções/Informações extras para cada chamada
+
+
+# 
--- script.js
+++ script.js
@@ -5,17 +5,62 @@ const askButton = document.getElementById('askButton')
 const aiResponse = document.getElementById('aiResponse')
 const form = document.getElementById('form')
 
-const enviarFormulario = (event) => {
+//AIzaSyComSK4_RBxG1ISfjagMZX2PMQvhnR-76Q
+const perguntarAI = async (question, game, apiKey) => {
+    const model = "gemini-2.5-flash"
+    const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
+    const pergunta = `
+        olha, tenho esse jogo ${game} e queria saber ${question}
+    `
+    
+    const contents = [{
+        parts: [{
+            text: pergunta
+        }]
+    }]
+
+    // chamada API
+    const response = await fetch(geminiURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'aplication/json'
+        },
+        body: JSON.stringify({
+            contents
+        })
+    })
+
+    const data = await response.json()
+    return data.candidates[0].content.parts[0].text
+
+}
+
+const enviarFormulario = async (event) => {
     event.preventDefault()
     const apiKey = apiKeyInput.value 
     const game = gameSelect.value
     const question = questionInput.value
 
 
-    console.log({apiKey, game, question})
+    if(apiKey == '' || game == ''|| question == '') {
+        alert('Por favor, preencha todos os campos')
+        return
+    }
 
-    if(true) {
+    askButton.disabled = true
+    askButton.textContent = 'Perguntando...'
+    askButton.classList.add('loading')
 
+    try {
+        //perguntar para AI
+      const text = await perguntarAI(question, game, apiKey)
+      aiResponse.querySelector('.response-content').innerHTML = text
+    } catch(error) {
+        console.log('Erro: ', error)
+    } finally {
+        askButton.disabled = false
+        askButton.textContent = "Perguntar"
+        askButton.classList.remove('loading')
     }
 
 }
