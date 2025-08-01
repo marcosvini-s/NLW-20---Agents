dbfcbbf4e5fe201182f3dc5dcf7c068ad480e7df
Marcos Vinicius <159066522+marcosvini-s@users.noreply.github.com>
2025-08-01 14:27:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const markdownToHTML = (text) =>{
     return converter.makeHtml(text)
 }
 
-//AIzaSyComSK4_RBxG1ISfjagMZX2PMQvhnR-76Q
 const perguntarAI = async (question, game, apiKey) => {
     const model = "gemini-2.5-flash"
     const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
